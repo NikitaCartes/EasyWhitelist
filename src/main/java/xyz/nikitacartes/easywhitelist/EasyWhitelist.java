b614fefffe072ef9bf9c8418cd48a6e432f0481d
NikitaCartes <nikich98@yandex.ru>
2022-05-21 16:55:13 +0300

    Update to 1.19-pre1

--- src/main/java/xyz/nikitacartes/easywhitelist/EasyWhitelist.java
+++ src/main/java/xyz/nikitacartes/easywhitelist/EasyWhitelist.java
@@ -2,8 +2,8 @@ package xyz.nikitacartes.easywhitelist;
 
 import com.mojang.authlib.GameProfile;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
-import net.minecraft.entity.player.PlayerEntity;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.util.dynamic.DynamicSerializableUuid;
 import org.apache.logging.log4j.LogManager;
 import xyz.nikitacartes.easywhitelist.commands.*;
 
@@ -13,14 +13,14 @@ import java.util.Collections;
 public class EasyWhitelist implements ModInitializer {
 
     public static Collection<GameProfile> getProfileFromNickname(String name) {
-        return Collections.singletonList(new GameProfile(PlayerEntity.getOfflinePlayerUuid(name), name));
+        return Collections.singletonList(new GameProfile(DynamicSerializableUuid.getOfflinePlayerUuid(name), name));
     }
 
     @Override
     public void onInitialize() {
         LogManager.getLogger().info("[EasyWhitelist] Whitelist is now name-based.");
 
-        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
+        CommandRegistrationCallback.EVENT.register((dispatcher, dedicated, environment) -> {
             EasyWhitelistCommand.registerCommand(dispatcher);
             EasyBanCommand.registerCommand(dispatcher);
             EasyPardonCommand.registerCommand(dispatcher);
