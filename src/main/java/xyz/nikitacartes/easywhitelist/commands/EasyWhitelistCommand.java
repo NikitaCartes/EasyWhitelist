ca7941082248762b21a1d06be2dac7e6bb5222ac
liu-zhou-L <61187892+liu-zhou-L@users.noreply.github.com>
2025-06-08 10:21:36 +0800

    Update EasyWhitelistCommand.java

--- src/main/java/xyz/nikitacartes/easywhitelist/commands/EasyWhitelistCommand.java
+++ src/main/java/xyz/nikitacartes/easywhitelist/commands/EasyWhitelistCommand.java
@@ -7,6 +7,7 @@ import me.lucko.fabric.api.permissions.v0.Permissions;
 
 import static com.mojang.brigadier.arguments.StringArgumentType.getString;
 import static com.mojang.brigadier.arguments.StringArgumentType.word;
+import static com.mojang.brigadier.arguments.StringArgumentType.string;
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 import static net.minecraft.server.dedicated.command.WhitelistCommand.executeAdd;
@@ -20,7 +21,7 @@ public class EasyWhitelistCommand {
                 .requires(Permissions.require("easywhitelist.commands.easywhitelist.root", 3))
                 .then(literal("add")
                         .requires(Permissions.require("easywhitelist.commands.easywhitelist.add", 3))
-                        .then(argument("targets", word())
+                        .then(argument("targets", string())
                                 .executes(ctx ->
                                         executeAdd(ctx.getSource(), getProfileFromNickname(getString(ctx, "targets")))
                                 )
